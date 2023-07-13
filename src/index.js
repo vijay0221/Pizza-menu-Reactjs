475c7cf3d3578a22e1400757d8f17b080f7b4542
vijay0221 <vijaymaroju2003@gmail.com>
2023-07-13 23:39:47 +0530

    index.js inital commits

---
+++ src/index.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+import './index.css';
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+function App(){
+  return <div className='container'>
+  <Header/> 
+  <Menu/>
+  <Footer/>
+
+  </div>
+       
+}
+
+function Header(){
+
+  const style={}
+
+  return <header className='header'> <h1>Fast React Pizza Co.</h1> </header>
+  
+}
+
+function Menu(){
+return <main className='menu'>
+          <h2>Our Menu</h2>
+          <Pizza name="Pizza Salamino"
+                 ingredients="Tomato, mozarella, and pepperoni"
+                 photoName="pizzas/salamino.jpg"
+                 price={10}
+          />
+          <Pizza name="Pizza Funghi"
+                 ingredients="Tomato, mushrooms"
+                 price={12}
+                 photoName="pizzas/funghi.jpg"/>
+         
+       </main>
+}
+
+function Pizza(props){
+  console.log(props)
+  return <div className='pizza'>
+    <img src={props.photoName} alt={props.name}/>
+     <div>
+       <h3>{props.name}</h3>
+       <p>{props.ingredients}</p>
+       <span>{props.price}</span>
+     </div>
+  </div>
+         
+}
+
+
+function Footer(){
+  const hour= new Date().getHours();
+  const openHour=8;
+  const closeHour=22;
+  const isOpen = hour >=openHour && hour<=closeHour;
+  // if(hour>=openHour && hour<=closeHour) alert('We are currently open!');
+  //  else alert('we are closed')
+  console.log(isOpen)
+  // return React.createElement('footer',null,"We're currently open !")
+  return <footer className='footer'> {new Date().toLocaleTimeString()} We are currently open!</footer>
+
+}
+
+
+
+//Render root (react v18)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App/>)
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>)
+
+//before react 18 
+//directly we used root.render(<App />,document.getElementById('root'))
