23dd62c3419871d5ead0ca8494f36dacfddc7360
vijay0221 <vijaymaroju2003@gmail.com>
2023-07-16 00:29:20 +0530

    rendering Lists

--- src/index.js
+++ src/index.js
@@ -69,7 +69,16 @@ function Header(){
 function Menu(){
 return <main className='menu'>
           <h2>Our Menu</h2>
-          <Pizza name="Pizza Salamino"
+
+          <ul className="pizzas">
+             
+             {
+              pizzaData.map(
+                pizza=> < Pizza pizzaObj={pizza} key={pizza.name}/>
+              )
+             }
+          </ul>
+          {/* <Pizza name="Pizza Salamino"
                  ingredients="Tomato, mozarella, and pepperoni"
                  photoName="pizzas/salamino.jpg"
                  price={10}
@@ -77,21 +86,21 @@ return <main className='menu'>
           <Pizza name="Pizza Funghi"
                  ingredients="Tomato, mushrooms"
                  price={12}
-                 photoName="pizzas/funghi.jpg"/>
+                 photoName="pizzas/funghi.jpg"/> */}
          
        </main>
 }
 
 function Pizza(props){
   console.log(props)
-  return <div className='pizza'>
-    <img src={props.photoName} alt={props.name}/>
+  return <list className='pizza'>
+    <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}/>
      <div>
-       <h3>{props.name}</h3>
-       <p>{props.ingredients}</p>
-       <span>{props.price}</span>
+       <h3>{props.pizzaObj.name}</h3>
+       <p>{props.pizzaObj.ingredients}</p>
+       <span>{props.pizzaObj.price}</span>
      </div>
-  </div>
+  </list>
          
 }
 
